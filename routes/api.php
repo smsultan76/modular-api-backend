4aa86c5ca5887d31cb2934e3c5ec22e98a04c61a
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2025-11-17 21:59:31 +0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PushNotificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,7 @@ Route::group(['prefix' => 'v1'], function () {
     // Route::group([], module_path('Profile', '/Routes/api.php'));
     // Route::group([], module_path('Blog', '/Routes/api.php'));
 });
+
+
+Route::post('/save-subscription', [PushNotificationController::class, 'save']);
+Route::post('/send-notification', [PushNotificationController::class, 'send']);
